b9305f1e52932f5475a551e9f140f58aca9385a8
zyayoung <1328410180@qq.com>
2019-02-15 13:51:45 +0800

    Update: UI

--- PlayListManager.py
+++ PlayListManager.py
@@ -15,6 +15,32 @@ import glob
 var_set = json.load(open('config.json'))
 
 
+class JsonDB:
+    def __init__(self):
+        if not os.path.exists("db.json"):
+            f = open("db.json", "w")
+            json.dump([], f)
+            f.close()
+            self.objects = []
+        else:
+            f = open("db.json", 'r')
+            self.objects = json.load(f)
+            f.close()
+
+    def save(self):
+        f = open("db.json", "w")
+        json.dump(self.objects, f)
+        f.close()
+
+    def append(self, obj):
+        self.objects.append(obj)
+
+    def get_object_by_key(self, key, value):
+        for obj in self.objects:
+            if obj[key] == value:
+                return obj
+
+
 class PlayListManager:
     def __init__(self):
         print('Initializing play list...')
@@ -23,13 +49,16 @@ class PlayListManager:
         self.q_new_song = Queue()
         self.play_next = False
         self.pause = False
-        self.ffserver = subprocess.Popen(
-                    ["ffserver", "-f", var_set['ffserver_config']],
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.STDOUT,
-                    universal_newlines=True
-                )
-
+        self.db = JsonDB()
+        try:
+            self.ffserver = subprocess.Popen(
+                        ["ffserver", "-f", var_set['ffserver_config']],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT,
+                        universal_newlines=True
+                    )
+        except FileNotFoundError:
+            print("ffserver not found!")
         # load local playlist
         if os.path.exists(var_set['download_path']):
             self.song_path = os.path.join(var_set['download_path'], 'song')
@@ -40,20 +69,18 @@ class PlayListManager:
             else:
                 os.mkdir(self.song_path)
 
-            self.pic_path = os.path.join(var_set['download_path'], 'pic')
-            if not os.path.exists(self.pic_path):
-                os.mkdir(self.pic_path)
-
         else:
             os.mkdir(var_set['download_path'])
             self.song_path = os.path.join(var_set['download_path'], 'song')
             os.mkdir(self.song_path)
-            self.pic_path = os.path.join(var_set['download_path'], 'pic')
-            os.mkdir(self.pic_path)
             self.add_song_by_id(521416315)
 
         self.player = td.Thread(target=self._player, name='Player')
         self.player.start()
+
+    def next(self):
+        self.play_next = True
+        return
 
     def add_song_by_name(self, name):
         try:
@@ -78,10 +105,10 @@ class PlayListManager:
                 self.play_list_ids.append(int(file_name[:10]))
 
             # check id
-            if song_id in self.play_list_ids:
-                for file in glob.glob(os.path.join(self.song_path, '%010d' % song_id + '*')):
-                    self.q_new_song.put(os.path.split(file)[1])
-                    self.play_next = True
+            old_obj = self.db.get_object_by_key('song_id', song_id)
+            if old_obj:
+                self.q_new_song.put(old_obj)
+                self.play_next = True
                 return
 
             # get song url
@@ -101,41 +128,34 @@ class PlayListManager:
                 print(detail_info)
                 return
             song_name = detail_info['songs'][0]['name']
-            pic_url = detail_info['songs'][0]['al']['picUrl']
 
-            # download song & pic
-            mp3_file_name = '%010d' % song_id + ' ' + song_name + '.mp3'
-            jpg_file_name = '%010d' % song_id + ' ' + song_name + '.jpg'
+            # download song
+            mp3_file_name = '%012d' % song_id + '.mp3'
 
             print('Downloading...')
             print('songName: ' + song_name)
             print('songUrl: ' + song_url)
-            print('picUrl: ' + pic_url)
             urlretrieve(song_url, os.path.join(self.song_path, mp3_file_name))
-            urlretrieve(pic_url, os.path.join(self.pic_path, jpg_file_name))
-
-            # resize img
-            print('resizing image')
-            img = Image.open(os.path.join(self.pic_path, jpg_file_name))
-            w, h = img.size
-            if h > 720 or w > 1280:
-                ratio = min(720.0/h, 1280.0/w)
-                img.thumbnail((int(h*ratio), int(w*ratio)))
-            img = img.convert('RGB')
-            img.save(os.path.join(self.pic_path, jpg_file_name), 'jpeg')
-
-            print('done')
+            new_song_obj = {
+                'mp3_file_name': mp3_file_name,
+                'song_url': song_url,
+                'song_name': song_name,
+                'song_id': song_id,
+                'states': 'downloaded',
+            }
+            self.db.append(new_song_obj)
+            self.db.save()
 
             # push q_new_song
-            self.q_new_song.put(mp3_file_name)
+            self.q_new_song.put(new_song_obj)
             self.play_list_ids.append(song_id)
-            self.play_next = True
+            self.next()
         except Exception as e:  # 防炸
             print('shit')
             print(e)
 
     def del_song_by_id(self, song_id):
-        # del song & pic files
+        # del song files
         try:
             self.file_names = os.listdir(self.song_path)
             self.play_list_ids = []
@@ -145,8 +165,6 @@ class PlayListManager:
             if song_id in self.play_list_ids:
                 for file in glob.glob(os.path.join(self.song_path, '%010d' % song_id + '*')):
                     os.remove(file)
-                for file in glob.glob(os.path.join(self.pic_path, '%010d' % song_id + '*')):
-                    os.remove(file)
             print('Deleted')
         except Exception as e:  # 防炸
             print('shit')
@@ -155,20 +173,21 @@ class PlayListManager:
     def _player(self):
         while True:
             song_path = os.path.join(var_set['download_path'], 'song')
-            if os.path.exists(song_path):
-                playlist = os.listdir(song_path)
+            playlist = os.listdir(song_path)
 
             # get next song
             if not self.q_new_song.empty():
-                nxt_song_filename = self.q_new_song.get()
-                if nxt_song_filename not in playlist:
-                    nxt_song_filename = random.choice(playlist)
+                nxt_song = self.q_new_song.get()
+                if nxt_song not in playlist:
+                    nxt_song = random.choice(self.db.objects)
             else:
-                nxt_song_filename = random.choice(playlist)
+                nxt_song = random.choice(self.db.objects)
 
-            if nxt_song_filename:
+            if nxt_song:
                 song_path = os.path.join(var_set['download_path'], 'song')
-                mp3_file_path = os.path.join(song_path, nxt_song_filename)
+                mp3_file_path = os.path.join(song_path, nxt_song['mp3_file_name'])
+
+                print(["ffmpeg", "-re", "-i", mp3_file_path, "http://127.0.0.1:8090/feed1.ffm"])
 
                 p = subprocess.Popen(
                     ["ffmpeg", "-re", "-i", mp3_file_path, "http://127.0.0.1:8090/feed1.ffm"],
@@ -178,10 +197,12 @@ class PlayListManager:
                 )
                 while p.poll() is None:
                     time.sleep(1)
-                    if self.play_next:
+                    try:
+                        if self.play_next:
+                            p.send_signal(2)
+                            self.play_next = False
+                    except ValueError:
+                        p.kill()
                         self.play_next = False
-                        p.send_signal(2)
-                if p.poll() == 1:
-                    print(p.stdout.read())
                 print('FFmpeg Ended with code', p.poll())
                 p.kill()
--- musicChooser.py
+++ musicChooser.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from queue import Queue
 import argparse
 import sys
 import random
 import string
 import logging
-import glob
+import json
 import PlayListManager
 
 play_list_manager = PlayListManager.PlayListManager()
@@ -15,31 +15,17 @@ SECRET_KEY = 'BoYanZhhhh'
 app = Flask(__name__)
 
 q = Queue()
+commandQ = Queue()
+data_list = []
+play_list = []
 
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
+# log = logging.getLogger('werkzeug')
+# log.setLevel(logging.ERROR)
 
 
-# identify user's identity
 @app.route("/")
 def hello():
-    return "Hello World!"
-
-
-@app.route("/music/api")
-def musicApi():
-    try:
-        if request.args.get('secretKey') != SECRET_KEY or \
-           request.args.get('action') is None:
-            raise Exception('invalid')
-        action = request.args.get('action')
-        if action == 'get':
-            if q.empty():
-                return 'Empty queue'
-            else:
-                return q.get()
-    except Exception as e:
-        return 'Request Error:' + str(e)
+    return render_template('index.html')
 
 
 @app.route("/add_by_id")
@@ -53,40 +39,53 @@ def add_by_name():
     play_list_manager.add_song_by_name(request.args.get('name'))
     return 'OK'
 
+
 @app.route("/next")
 def next():
     play_list_manager.play_next = True
     return 'OK'
 
 
+@app.route('/music/list')
+def musicList():
+    re = ''
+    for obj in play_list_manager.db.objects:
+        re += '{}<br/>'.format(obj)
+    return re
+
+
 @app.route('/music')
 def music():
-    re = ''
-    if (request.args.get('id') is not None):
-        re += '<a href="/music">Back</a>'
+    re = '<p><a href="?command=start">start</a>&nbsp; \
+             <a href="?command=pause">pause</a>&nbsp; \
+             <a href="?command=next">next</a>&nbsp; \
+             <a href="?command=vup">vup</a>&nbsp; \
+             <a href="?command=vdown">vdown</a></p>'
+    for obj in play_list_manager.db.objects:
+        re += '<p>{id}.<a href="?id={id}">{name}</a></p>\n'.format(
+            id=obj['song_id'], name=obj['song_name'])
+    if request.args.get('id') is not None:
         id = int(request.args.get('id'))
-        fileList = glob.glob(r'*.mp3')
-        if id >= 1 and id <= len(fileList):
-            q.put(str(id))
-    else:
-        id = 1
-        for fileName in glob.glob(r'*.mp3'):
-            re += '<p>' + str(id) + '. ' + \
-               '<a href="?id=' + str(id) + '">' + fileName[:-4] + '</a>' + \
-               '</p>\n'
-            id += 1
-    return re
+        play_list_manager.add_song_by_id(id)
+    if request.args.get('command') is not None:
+        command = request.args.get('command')
+        if command == 'next':
+            next()
+        elif command in ['start', 'pause', 'next', 'vup', 'vdown']:
+            commandQ.put(command)
+            re += '<p>{}</p>'.format(command)
+    return render_template('music.html', re=re, data_list=play_list_manager.db.objects)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Music Chooser Server.')
     parser.add_argument('-ip', default='0.0.0.0', type=str)
-    parser.add_argument('-port', default='5000', type=str)
+    parser.add_argument('-port', default='5001', type=str)
     parser.add_argument('-sk', default='BoYanZhhhh', type=str)
     args = vars(parser.parse_args())
     SECRET_KEY = args['sk']
     if SECRET_KEY == 'YourSecretKey':
         SECRET_KEY = ''.join(
             random.sample(string.ascii_letters + string.digits, 8))
-    print('Your Secret Key: ' + SECRET_KEY)
+    print('start')
     app.run(host=args['ip'], port=args['port'])
